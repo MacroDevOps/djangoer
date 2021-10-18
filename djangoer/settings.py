00c719ae0ccec7424f3fff3ed1764ea49ca387a0
Durgin <macroldj@gmail.com>
2021-10-18 19:21:41 +0800

    feat (actions status) python uwsgi 集成部署方案-解决静态资源挂在问题

--- djangoer/settings.py
+++ djangoer/settings.py
@@ -138,11 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 # Redis cache configure
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
