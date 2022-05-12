9b62924e51f73ed895ebb280c1355ef2591b8220
Durgin <macroldj@gmail.com>
2022-05-13 00:09:41 +0800

    ci(debug docker-compose) debug compose for docker service debug flower

--- flowerconfig.py
+++ flowerconfig.py
@@ -1,5 +1,11 @@
 # RabbitMQ management api
-broker_api = 'http://admin:admin@localhost:15672/api/'
+import os
+
+broker_api = 'http://{RABBITMQ_USER}:{RABBITMQ_PASSWD}@{RABBITMQ_IP}:5672/api/' \
+    .format(RABBITMQ_IP=os.environ.get("RABBITMQ_IP"),
+            RABBITMQ_USER=os.environ.get("RABBITMQ_USER"),
+            RABBITMQ_PASSWD=os.environ.get("RABBITMQ_PASSWD")
+            )
 
 # Enable debug logging
 logging = 'DEBUG'
